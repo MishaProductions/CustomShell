e73e33e1429d9aea256bfe9f5c15123574070ac0
Mikhail <mishakeys20@gmail.com>
2024-09-09 20:47:56 -0400

    begin work on removing IAM api dependency

--- CustomShell/main.cpp
+++ CustomShell/main.cpp
@@ -9,10 +9,214 @@
 #include "CustomShell.h"
 #include <iostream>
 
+
+typedef HWND(WINAPI* CreateWindowInBandAPI)(DWORD, LPWSTR, PVOID, PVOID, PVOID, PVOID, PVOID, PVOID, PVOID, PVOID, PVOID, PVOID, DWORD);
+static CreateWindowInBandAPI CreateWindowInBandOrig;
+
+typedef HWND(WINAPI* CreateWindowInBandExAPI)(DWORD, LPWSTR, PVOID, PVOID, PVOID, PVOID, PVOID, PVOID, PVOID, PVOID, PVOID, PVOID, DWORD, DWORD);
+static CreateWindowInBandExAPI CreateWindowInBandExOrig;
+
+typedef BOOL(WINAPI* GetWindowBandAPI)(HWND, DWORD*);
+static GetWindowBandAPI GetWindowBandOrig;
+
+typedef HWND(WINAPI* SetWindowBandApi)(HWND hwnd, HWND hwndInsertAfter, DWORD dwBand);
+static SetWindowBandApi SetWindowBandApiOrg;
+
+typedef BOOL(WINAPI* RegisterHotKeyApi)(HWND hwnd, int id, UINT fsMod, UINT vk);
+static RegisterHotKeyApi RegisterHotKeyApiOrg;
+
+HWND WINAPI CreateWindowInBandNew(DWORD dwExStyle,
+	LPCWSTR lpClassName,
+	LPCWSTR lpWindowName,
+	DWORD dwStyle,
+	int x,
+	int y,
+	int nWidth,
+	int nHeight,
+	HWND hWndParent,
+	HMENU hMenu,
+	HINSTANCE hInstance,
+	LPVOID lpParam,
+	DWORD dwBand)
+{
+	DWORD p0 = (DWORD)_ReturnAddress();
+	printf("CreateWindowInBandNew\n");
+	HWND ret = CreateWindowExW(dwExStyle, lpClassName, lpWindowName, dwStyle, x, y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+
+	if (ret)
+		SetProp(ret, L"UIA_WindowVisibilityOverriden", (HANDLE)2);
+	return ret;
+}
+
+HWND WINAPI CreateWindowInBandExNew(DWORD dwExStyle,
+	LPCWSTR lpClassName,
+	LPCWSTR lpWindowName,
+	DWORD dwStyle,
+	int x,
+	int y,
+	int nWidth,
+	int nHeight,
+	HWND hWndParent,
+	HMENU hMenu,
+	HINSTANCE hInstance,
+	LPVOID lpParam,
+	DWORD dwBand, DWORD dwTypeFlags)
+{
+	DWORD p0 = (DWORD)_ReturnAddress();
+	printf("CreateWindowInBandExNew\n");
+	HWND ret = CreateWindowExW(dwExStyle, lpClassName, lpWindowName, dwStyle, x, y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+	SetProp(ret, L"UIA_WindowVisibilityOverriden", (HANDLE)2);
+	return ret;
+}
+
+BOOL WINAPI SetWindowBandNew(HWND hwnd, HWND hwndInsertAfter, DWORD flags)
+{
+	printf("SetWindowBandNew\n");
+	return TRUE;
+}
+
+BOOL WINAPI ReturnZero()
+{
+	return TRUE;
+}
+
+BOOL WINAPI RegisterWindowHotkeyNew(HWND hwnd, int id, UINT mod, UINT vk)
+{
+	BOOL res = RegisterHotKeyApiOrg(hwnd, id, mod, vk);
+
+	if (!res)
+	{
+		printf("RegisterWindowHotkeyNew did things\n");
+		return TRUE;
+	}
+
+	return TRUE;
+}
+
+BOOL GetWindowTrackInfoAsyncStub(HWND handle, HWND h2)
+{
+	printf("GetWindowTrackInfoAsyncStub\n");
+
+	BOOL res2= ChangeWindowMessageFilterEx(handle, 834, MSGFLT_ALLOW, NULL);
+	HRESULT hr = GetLastError();
+	BOOL b = PostMessageW(handle, 834, 1, 0);
+	return TRUE;
+}
+
+BOOL ClearForegroundStub()
+{
+	printf("ClearForegroundStub\n");
+	return TRUE;
+}
+
+BOOL CreateWindowGroupStub()
+{
+	printf("CreateWindowGroupStub\n");
+	return TRUE;
+}
+
+BOOL DeleteWindowGroupStub()
+{
+	printf("DeleteWindowGroupStub\n");
+	return TRUE;
+}
+
+BOOL EnableWindowGroupPolicyStub()
+{
+	printf("EnableWindowGroupPolicyStub\n");
+	return TRUE;
+}
+
+BOOL SetBridgeWindowChildStub()
+{
+	printf("SetBridgeWindowChildStub\n");
+	return TRUE;
+}
+BOOL SetFallbackForegroundStub()
+{
+	printf("SetFallbackForegroundStub\n");
+	return TRUE;
+}
+BOOL SetWindowArrangementStub()
+{
+	printf("SetWindowArrangementStub\n");
+	return TRUE;
+}
+BOOL SetWindowGroupStub()
+{
+	printf("SetWindowGroupStub\n");
+	return TRUE;
+}
+
+BOOL SetWindowShowStateStub()
+{
+	printf("SetWindowShowStateStub\n");
+	return TRUE;
+}
+
+BOOL UpdateWindowTrackingInfoStub()
+{
+	printf("UpdateWindowTrackingInfoStub\n");
+	return TRUE;
+}
+
+BOOL AllowSetForegroundWindowStub()
+{
+	printf("AllowSetForegroundWindowStub\n");
+	return TRUE;
+}
+
+
+void hookApis()
+{
+	printf("hook IAM api\n");
+	CreateWindowInBandOrig = decltype(CreateWindowInBandOrig)(GetProcAddress(GetModuleHandle(L"user32.dll"), "CreateWindowInBand"));
+	CreateWindowInBandExOrig = decltype(CreateWindowInBandExOrig)(GetProcAddress(GetModuleHandle(L"user32.dll"), "CreateWindowInBandEx"));
+	SetWindowBandApiOrg = decltype(SetWindowBandApiOrg)(GetProcAddress(GetModuleHandle(L"user32.dll"), "SetWindowBand"));
+	RegisterHotKeyApiOrg = decltype(RegisterHotKeyApiOrg)(GetProcAddress(GetModuleHandle(L"user32.dll"), "RegisterHotKey"));
+
+	DetourTransactionBegin();
+	DetourUpdateThread(GetCurrentThread());
+	DetourAttach((LPVOID*)&CreateWindowInBandOrig, (PVOID)CreateWindowInBandNew);
+	DetourAttach((LPVOID*)&CreateWindowInBandExOrig, (PVOID)CreateWindowInBandExNew);
+	DetourAttach((LPVOID*)&SetWindowBandApiOrg, (PVOID)SetWindowBandNew);
+	DetourAttach((LPVOID*)&RegisterHotKeyApiOrg, (PVOID)RegisterWindowHotkeyNew);
+
+
+	PVOID junk = GetProcAddress(GetModuleHandle(L"user32.dll"), (LPCSTR)2581); // GetWindowTrackInfoAsync
+	DetourAttach((LPVOID*)&junk, (PVOID)GetWindowTrackInfoAsyncStub);
+	junk = GetProcAddress(GetModuleHandle(L"user32.dll"), (LPCSTR)2563); // ClearForeground
+	DetourAttach((LPVOID*)&junk, (PVOID)ClearForegroundStub);
+	junk = GetProcAddress(GetModuleHandle(L"user32.dll"), (LPCSTR)2628); // CreateWindowGroup
+	DetourAttach((LPVOID*)&junk, (PVOID)CreateWindowGroupStub);
+	junk = GetProcAddress(GetModuleHandle(L"user32.dll"), (LPCSTR)2629); // DeleteWindowGroup
+	DetourAttach((LPVOID*)&junk, (PVOID)DeleteWindowGroupStub);
+	junk = GetProcAddress(GetModuleHandle(L"user32.dll"), (LPCSTR)2631); //EnableWindowGroupPolicy
+	DetourAttach((LPVOID*)&junk, (PVOID)EnableWindowGroupPolicyStub);
+	junk = GetProcAddress(GetModuleHandle(L"user32.dll"), (LPCSTR)2627); // SetBridgeWindowChild
+	DetourAttach((LPVOID*)&junk, (PVOID)SetBridgeWindowChildStub);
+	junk = GetProcAddress(GetModuleHandle(L"user32.dll"), (LPCSTR)2511); // SetFallbackForeground
+	DetourAttach((LPVOID*)&junk, (PVOID)SetFallbackForegroundStub);
+	junk = GetProcAddress(GetModuleHandle(L"user32.dll"), (LPCSTR)2566); // SetWindowArrangement
+	DetourAttach((LPVOID*)&junk, (PVOID)SetWindowArrangementStub);
+	junk = GetProcAddress(GetModuleHandle(L"user32.dll"), (LPCSTR)2632); // SetWindowGroup
+	DetourAttach((LPVOID*)&junk, (PVOID)SetWindowGroupStub);
+	junk = GetProcAddress(GetModuleHandle(L"user32.dll"), (LPCSTR)2579); // SetWindowShowState
+	DetourAttach((LPVOID*)&junk, (PVOID)SetWindowShowStateStub);
+	junk = GetProcAddress(GetModuleHandle(L"user32.dll"), (LPCSTR)2585); // UpdateWindowTrackingInfo
+	DetourAttach((LPVOID*)&junk, (PVOID)UpdateWindowTrackingInfoStub);
+	junk = GetProcAddress(GetModuleHandle(L"user32.dll"), "AllowSetForegroundWindow");
+	DetourAttach((LPVOID*)&junk, (PVOID)AllowSetForegroundWindowStub);
+
+	DetourTransactionCommit();
+
+	printf("hooked IAM apis\n");
+}
+
 int MainHook(
-	 HINSTANCE hInstance,
-	 HINSTANCE hPrevInstance,
-	 LPSTR     lpCmdLine,
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
 	int       nShowCmd
 )
 {
@@ -25,6 +229,8 @@ int MainHook(
 	}
 	printf("Starting it\n");
 
+	hookApis();
+
 	//create the shell object
 	CustomShell* object = new CustomShell();
 	if (FAILED(object->Run()))
@@ -34,6 +240,13 @@ int MainHook(
 	system("pause");
 
 	return -1;
+}
+
+int main()
+{
+	hookApis();
+	MainHook(GetModuleHandleA(NULL), NULL, NULL, 0);
+	return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
